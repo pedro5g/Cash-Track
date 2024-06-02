bdc7ebe068ec02a8fb568291a7481f257ab22fcd
Pedr0Martins13 <pedluiz321@gmail.com>
2024-06-01 22:09:33 -0300

    feat: add QueryClientProvider

--- components/providers/root-providers.tsx
+++ components/providers/root-providers.tsx
@@ -1,15 +1,22 @@
 "use client";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "next-themes";
+import { useState } from "react";
 
 export const RootProvider = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="dark"
-      enableSystem
-      disableTransitionOnChange>
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
