0f98dab8cc5179f923606d13ce566293a69ef6b4
Pedr0Martins13 <pedluiz321@gmail.com>
2024-06-01 22:08:45 -0300

    feat add and set database end prisma orm

---
+++ prisma/migrations/20240601214332_first_migrate_set_main_tables/migration.sql
@@ -0,0 +1,54 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "userId" TEXT NOT NULL PRIMARY KEY,
+    "currency" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "category" (
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "icon" TEXT NOT NULL,
+    "type" TEXT NOT NULL DEFAULT 'income'
+);
+
+-- CreateTable
+CREATE TABLE "transaction" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "update_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "amount" REAL NOT NULL,
+    "description" TEXT NOT NULL,
+    "date" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL DEFAULT 'income',
+    "category" TEXT NOT NULL,
+    "categoryIcon" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "month_history" (
+    "userId" TEXT NOT NULL,
+    "day" INTEGER NOT NULL,
+    "month" INTEGER NOT NULL,
+    "year" INTEGER NOT NULL,
+    "income" REAL NOT NULL,
+    "expense" REAL NOT NULL,
+
+    PRIMARY KEY ("day", "month", "year", "userId")
+);
+
+-- CreateTable
+CREATE TABLE "year_history" (
+    "userId" TEXT NOT NULL,
+    "month" INTEGER NOT NULL,
+    "year" INTEGER NOT NULL,
+    "income" REAL NOT NULL,
+    "expense" REAL NOT NULL,
+
+    PRIMARY KEY ("month", "year", "userId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "category_name_userId_type_key" ON "category"("name", "userId", "type");
